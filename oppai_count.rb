a80d95cd9be4f4dc5057f4a3ef4394859f8ec5a1
ryoana14 <anana12185@gmail.com>
2017-05-18 21:02:16 +0900

    おっぱい語録の実装とちょっとDRY

--- oppai_count.rb
+++ oppai_count.rb
@@ -3,6 +3,15 @@ require 'json'
 require 'yaml'
 require 'eventmachine'
 require 'faye/websocket'
+require File.expand_path('../oppai_methods.rb', __FILE__)
+
+def judge(data, bot_id)
+  Oppai::Utils.oppai_judge(data, bot_id)
+end
+
+def oppai_method(fun)
+  Oppai::Utils.send(fun)
+end
 
 config = YAML.load_file("#{ENV['HOME']}/.oppai")
 res = HTTP.post("https://slack.com/api/rtm.start", params: {
@@ -24,18 +33,34 @@ EM.run do
 
   ws.on :message do |event|
     data = JSON.parse(event.data)
-    if data['text'] =~ /お.*っ.*ぱ.*い/ and data['user'] != config['bot_id'] and data['type'] == 'message'
+    if data['text'] =~ /お.*っ.*ぱ.*い/ and judge(data, config['bot_id'])
       cnt += 1
-    elsif data['text'] == "oppai count" and data['type'] == 'message' and data['user'] != config['bot_id']
+    elsif data['text'] == "oppai count" and judge(data, config['bot_id'])
       ws.send({
         type: 'message',
         text: "現在#{cnt}おっぱいです",
         channel: data['channel']
       }.to_json)
-    elsif data['text'] == "oppai help" and data['type'] == 'message' and data['user'] != config['bot_id']
+    elsif data['text'] == "oppai word" and judge(data, config['bot_id'])
       ws.send({
         type: 'message',
-        text: "Usage: oppai <subcommand>\n\soppai count : display sum of oppai.\n oppai help : display this message.",
+        text: oppai_method(:word),
+        channel: data['channel']
+      }.to_json)
+    elsif data['text'] == "oppai flag" and judge(data, config['bot_id'])
+      ws.send({
+        type: 'message',
+        text: oppai_method(:flag),
+        channel: data['channel']
+      }.to_json)
+    elsif data['text'] == "oppai help" and judge(data, config['bot_id'])
+      ws.send({
+        type: 'message',
+        text: "Usage: oppai <subcommand>\n\
+               oppai count : おっぱい数を報告します.\n\
+               oppai word  : おっぱい宣教師のありがたい語録を表示します.\n\
+               oppai flag  : おっぱいフラグをたてます.\n\
+               oppai help  : このヘルプを表示します.",
         channel: data['channel']
       }.to_json)
     end
