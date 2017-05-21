c2a630fa6cc6e2f0ad24818aaae9fbc89ce6dfa4
ryoana14 <anana12185@gmail.com>
2017-05-21 16:05:20 +0900

    りふぁくたりんぐ

---
+++ lib/oppai/utils.rb
@@ -0,0 +1,65 @@
+class Oppai
+  class Utils
+    class << self
+      # bot以外のユーザの発言かどうか判定
+      def op_judge(data, bot_id)
+        if data['user'] != bot_id and data['type'] == 'message'
+          true
+        else
+          false
+        end
+      end
+    end
+
+    attr_accessor :cnt, :mes_list
+
+    def initialize
+      @words = File.open(File.expand_path('./dicts/word.opp')).readlines.map(&:chomp)
+      @flags = File.open(File.expand_path('./dicts/flag.opp')).readlines.map(&:chomp)
+      @mes_list = []
+    end
+
+    def method_missing(name)
+      "`#{name}` なんてコマンドはないっぱい。出直してくるっぱい"
+    end
+
+    # おっぱい数を返す
+    def count
+      "現在 #{@cnt}おっぱいです"
+    end
+
+    # おっぱい宣教師語録+α
+    def word
+      @words.sample
+    end
+
+    # おっぱいフラグ
+    def flag
+      @flags.sample
+    end
+
+    def per
+      if @mes_list.size == 0
+        "おっぱわーが足りません"
+      else
+        begin
+          opper = @mes_list.reduce(0) { |sum, m| sum += m.scan(/お.*?っ.*?ぱ.*?い/).size }
+          per = 100 * opper / @mes_list.size
+          "現在のおっぱい濃度は #{per} %です"
+        rescue
+          p @mes_list
+          "`oppai per` は使えないっぱいです"
+        end
+      end
+    end
+
+    def help
+      "Usage: oppai <subcommand>\n\
+              oppai count\tおっぱい数を報告します.\n\
+              oppai word\tおっぱい宣教師のありがたい語録を表示します.\n\
+              oppai flag\tおっぱいフラグをたてます.\n\
+              oppai per\tチャンネル内のおっぱい濃度を表示します.(test)\n\
+              oppai help\tこのヘルプを表示します."
+    end
+  end
+end
--- oppai_count.rb
+++ oppai_count.rb
@@ -3,12 +3,7 @@ require 'json'
 require 'yaml'
 require 'eventmachine'
 require 'faye/websocket'
-require File.expand_path('../oppai_methods.rb', __FILE__)
-
-# 暫定メソッド
-def judge(data, bot_id)
-  Oppai::Utils.oppai_judge(data, bot_id)
-end
+require File.expand_path('../lib/oppai/utils.rb', __FILE__)
 
 # 諸設定
 
@@ -22,17 +17,16 @@ res = HTTP.post("https://slack.com/api/rtm.start", params: {
 rc = JSON.parse(res.body)
 url = rc['url']
 
-## 苦し紛れのおっぱい数継承
-if ARGV[0].nil?
-  cnt = 0
-else
-  cnt = ARGV[0].to_i
-end
-
 ## Oppai::Utilsのインスタンス作成
 op = Oppai::Utils.new
 
-mes_list = []
+## 苦し紛れのおっぱい数継承
+if ARGV[0].nil?
+  op.cnt = 0
+else
+  op.cnt = ARGV[0].to_i
+end
+
 
 # oppai_info本体
 EM.run do
@@ -41,54 +35,30 @@ EM.run do
   ws.on :message do |event|
     data = JSON.parse(event.data)
 
-    # おっぱいコマンドとbotのreplyを除く直近30件の発言を保存
-    if data['text'] !~ /^oppai [a-z]+$/ and judge(data, config['bot_id'])
-      if mes_list.size < 30
-        mes_list.push(data['text'])
-      else
-        mes_list.shift
-        mes_list.push(data['text'])
+    # botの発言はシカト
+    if Oppai::Utils.op_judge(data, config['bot_id'])
+      # おっぱいコマンドとbotのreplyを除く直近30件の発言を保存
+      if data['text'] !~ /^oppai [a-z]+$/
+        if op.mes_list.size < 30
+          op.mes_list.push(data['text'])
+        else
+          op.mes_list.shift
+          op.mes_list.push(data['text'])
+        end
       end
-    end
 
-    if data['text'] =~ /お.*?っ.*?ぱ.*?い/ and judge(data, config['bot_id'])
-      cnt += data['text'].scan(/お.*?っ.*?ぱ.*?い/).size
-    elsif data['text'] == "oppai count" and judge(data, config['bot_id'])
-      ws.send({
-        type: 'message',
-        text: "現在#{cnt}おっぱいです",
-        channel: data['channel']
-      }.to_json)
-    elsif data['text'] == "oppai word" and judge(data, config['bot_id'])
-      ws.send({
-        type: 'message',
-        text: op.word,
-        channel: data['channel']
-      }.to_json)
-    elsif data['text'] == "oppai flag" and judge(data, config['bot_id'])
-      ws.send({
-        type: 'message',
-        text: op.flag,
-        channel: data['channel']
-      }.to_json)
-    # おっぱい濃度(テスト版)
-    elsif data['text'] == "oppai per" and judge(data, config['bot_id'])
-      ws.send({
-        type: 'message',
-        text: op.per(mes_list),
-        channel: data['channel']
-      }.to_json)
-    elsif data['text'] == "oppai help" and judge(data, config['bot_id'])
-      ws.send({
-        type: 'message',
-        text: "Usage: oppai <subcommand>\n\
-               oppai count\tおっぱい数を報告します.\n\
-               oppai word\tおっぱい宣教師のありがたい語録を表示します.\n\
-               oppai flag\tおっぱいフラグをたてます.\n\
-               oppai per\tチャンネル内のおっぱい濃度を表示します.(test)\n\
-               oppai help\tこのヘルプを表示します.",
-        channel: data['channel']
-      }.to_json)
+      # おっぱい数のカウント
+      if data['text'] =~ /お.*?っ.*?ぱ.*?い/
+        op.cnt += data['text'].scan(/お.*?っ.*?ぱ.*?い/).size
+      # おっぱいコマンドの呼び出し
+      elsif data['text'] =~ /^oppai [a-z]+$/
+        oppai, cmd = data['text'].split(' ')
+        ws.send({
+          type: 'message',
+          text: op.send(cmd.intern),
+          channel: data['channel']
+        }.to_json)
+      end
     end
   end
 
