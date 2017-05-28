61edc1ea2675f44809e32dee8b9034a1029a274c
ryoana14 <anana12185@gmail.com>
2017-05-28 17:22:00 +0900

    version入れよう

--- lib/oppai.rb
+++ lib/oppai.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../lib/cmd.rb', __FILE__)
 require File.expand_path('../lib/data.rb', __FILE__)
 require File.expand_path('../lib/utils.rb', __FILE__)
+require File.expand_path('../lib/version.rb', __FILE__)
+
 class Oppai
 
 end
