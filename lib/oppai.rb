e343d0c5801fc4974770864cdb6175db9ed093b8
ryoana14 <anana12185@gmail.com>
2017-06-01 22:15:46 +0900

    require likely_keyword

--- lib/oppai.rb
+++ lib/oppai.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../oppai/cmd.rb', __FILE__)
+require File.expand_path('../oppai/cmd/likely_keyword.rb', __FILE__)
 require File.expand_path('../oppai/data.rb', __FILE__)
 require File.expand_path('../oppai/utils.rb', __FILE__)
 require File.expand_path('../oppai/version.rb', __FILE__)
