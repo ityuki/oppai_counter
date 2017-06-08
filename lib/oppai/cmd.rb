be6c646e54b32c13ac2c46d48e5251a7207259cf
ryoana14 <anana12185@gmail.com>
2017-06-08 21:09:50 +0900

    sleepコマンドを実装

--- lib/oppai/cmd.rb
+++ lib/oppai/cmd.rb
@@ -1,6 +1,6 @@
 class Oppai
   class Cmd
-    attr_reader :white_methods, :destroy_methods, :data, :likely
+    attr_reader :white_methods, :destroy_methods, :likely
 
     def initialize(data)
       @white_methods = {
@@ -9,14 +9,16 @@ class Oppai
         'flag'    => 'おっぱいフラグをたてます.',
         'hello'   => '色んな言語でhello worldを表示します.',
         'per'     => 'チャンネル内のおっぱい濃度を表示します.',
-        'random'  => 'どれかのコマンドを実行します',
+        'random'  => 'どれかのコマンドを実行します.',
+        'sleep'   => 'ラリホー！',
+        'wakeup'  => 'ザメハ！',
         'version' => 'oppai_infoのversionを表示します.',
         'help'    => 'このヘルプを表示します.'
       }
-      @destroy_methods = %w(abort throw raise fail exit sleep
+      @destroy_methods = %w(abort throw raise fail exit
                             inspect new clone initialize)
       @likely = LikelyKeyword.new(@white_methods.keys + @destroy_methods,@white_methods.keys)
-      @data = data
+      @oppai_data = data
     end
 
     def check_and_execute(sent_command)
@@ -44,22 +46,22 @@ class Oppai
 
     # おっぱい数を返す
     def count
-      "現在 #{data.oppai_count}おっぱいです"
+      "現在 #{@oppai_data.oppai_count}おっぱいです"
     end
 
     # おっぱい宣教師語録+α
     def word
-      data.words.sample
+      @oppai_data.words.sample
     end
 
     # おっぱいフラグ
     def flag
-      data.flags.sample
+      @oppai_data.flags.sample
     end
 
     # helloおっぱい
     def hello
-      data.hello.sample
+      @oppai_data.hello.sample
     end
 
     def random
@@ -72,12 +74,26 @@ class Oppai
     end
 
     def per
-      if data.message_list.size == 0
+      if @oppai_data.message_list.size == 0
         "おっぱわーが足りません"
       else
-        opper = data.message_list.reduce(0) { |sum, m| sum += m.scan(/お.*?っ.*?ぱ.*?い/).size }
-        per = 100 * opper / data.message_list.size
+        opper = @oppai_data.message_list.reduce(0) { |sum, m| sum += m.scan(/お.*?っ.*?ぱ.*?い/).size }
+        per = 100 * opper / @oppai_data.message_list.size
         "現在のおっぱい濃度は #{per} %です"
+      end
+    end
+
+    def sleep
+      @oppai_data.set_sleep_start_time
+      "おやぱい"
+    end
+
+    def wakeup
+      if @oppai_data.sleep_start_time.nil?
+        "起きてるっぱい！"
+      else
+        @oppai_data.set_sleep_start_time(nil)
+        "おはぱい"
       end
     end
 
